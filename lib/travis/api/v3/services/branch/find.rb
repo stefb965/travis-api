0ed3bb7dd257ed5755120ec23bcbc137a0ae6d77
carlad <carla@travis-ci.org>
2017-04-21 12:36:04 +0200

    fix params

--- lib/travis/api/v3/services/branch/find.rb
+++ lib/travis/api/v3/services/branch/find.rb
@@ -1,6 +1,7 @@
 module Travis::API::V3
   class Services::Branch::Find < Service
-    params :recent_builds, prefix: :branch
+    # params :recent_builds, prefix: :branch
+    params "branch.recent_builds"
     def run!
       result find(:branch, find(:repository))
     end
